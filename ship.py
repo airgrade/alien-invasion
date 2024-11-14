2d916f0782c83ef2c04e49269c569fb9e6272b7e
airgrade <charlesmugendi2020@gmail.com>
2024-11-14 12:12:57 +0300

    readme added

--- ship.py
+++ ship.py
@@ -10,8 +10,8 @@ class Ship:
         self.screen_rect = ai_game.screen.get_rect()
         
         #Load the ship and gets its rect.
-        self.image = pygame.image.load('images/ship0bmp')
-        self.rec = self.image.get_rect()
+        self.image = pygame.image.load('images/ship0.bmp')
+        self.rect = self.image.get_rect()
         
         #Start each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
