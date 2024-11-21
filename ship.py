2a94e06c99782338dfd612384be97b6d45afdf1a
airgrade <charlesmugendi2020@gmail.com>
2024-11-21 08:30:24 +0300

    Limiting the ships range

--- ship.py
+++ ship.py
@@ -27,11 +27,14 @@ class Ship:
     def update(self):
         """Upadate the ship's position based on the movement of the flag. """
         #updating the ship's x value and not the rect
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right :
             self.x += self.settings.ship_speed
             
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
+            
+        #update the rect object from self.x
+        self.rect.x = self.x
         
     def blitme(self):
         '''Draw the ship at its current location.'''
