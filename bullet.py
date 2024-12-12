e65b9bfeb8d729eb658fcbf0b5ff77a6da531630
airgrade <charlesmugendi2020@gmail.com>
2024-12-12 09:38:10 +0300

    updated the bullet to move up the screen

--- bullet.py
+++ bullet.py
@@ -19,3 +19,14 @@ class Bullet(Sprite):
         #Store bullets postion as a float
         self.y = float(self.rect.y)
     
+    def update(self):
+        '''move the bullet up the screen.'''
+        
+        #Update the exact position of the bullet.
+        self.y = self.settings.bullet_speed
+        #Update the rect position
+        self.rect.y = self.y
+    
+    def draw_bullet(self):
+        '''Draw the bullet on to the screen'''
+        pygame.draw.rect(self.screen, self.color, self.rect)
